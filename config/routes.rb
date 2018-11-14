d7e91fc2f89237e6fb0d96c0e1c6287fd0aaa57c
Michael Pieschel <sixtyhectares@yahoo.com>
2018-11-14 04:29:00 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root "static_pages#home"
   get "/signin", to: "sessions#new"
+  post '/signin', to: 'sessions#create'
   post "/sessions/create", to: "sessions#create"
   delete "/signout", to: "sessions#destroy"
   post "/rides/new", to: "rides#new"
