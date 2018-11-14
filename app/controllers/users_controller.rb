9e7a9a97c135836bd27695543d4e8eeb759737b1
Michael Pieschel <sixtyhectares@yahoo.com>
2018-11-14 02:46:43 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,29 +25,14 @@ class UsersController < ApplicationController
     end
   end
 
-  def edit
-    @user = User.find(params[:id])
-  end
-  def create
-    @user = User.create(user_params)
-    if !@user.save
-      redirect_to controller: 'users', action: 'new'
->>>>>>> e8377532b7eb71ab9d63994d832d74e2b92e9d61
-    else
-      session[:user_id] = @user.id
-      redirect_to user_path(@user)
-    end
-  end
-
   def update
       if @user.update(user_params)
+        notice: 'User was successfully updated.'
         redirect_to user_path(@user)
       else
-<<<<<<< HEAD
         render :edit
 =======
         redirect_to controller: 'users', action: 'edit'
->>>>>>> e8377532b7eb71ab9d63994d832d74e2b92e9d61
       end
   end
 
