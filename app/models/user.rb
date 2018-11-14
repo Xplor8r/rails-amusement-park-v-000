ec9e7e5c875e24bfae8e516c3d4d99c32a84546e
Michael Pieschel <sixtyhectares@yahoo.com>
2018-11-14 02:51:45 +0000

    automatically backed up by learn

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,16 @@ class User < ActiveRecord::Base
   has_many :rides
   has_many :attractions, through: :rides
   has_secure_password
+
+  def mood
+    if nausea <= happiness
+      "happy"
+    else
+      "sad"
+    end
+  end
+
+  def num_of_rides
+    attractions.count
+  end
 end
