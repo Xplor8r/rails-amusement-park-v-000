ab3e139eafbaddc9622e730d324777509bd4f20b
Michael Pieschel <sixtyhectares@yahoo.com>
2018-11-14 03:19:33 +0000

    automatically backed up by learn

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,8 @@ class User < ActiveRecord::Base
   has_many :attractions, through: :rides
   has_secure_password
 
-  def mood
-    if self.nausea <= self.happiness
+  def mood 
+    if nausea <= happiness
       "happy"
     else
       "sad"
