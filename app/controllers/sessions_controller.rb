4655eb9c24c9112f0548bcfc1af03667e0e3724e
Michael Pieschel <sixtyhectares@yahoo.com>
2018-11-14 02:42:02 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,16 +10,13 @@ class SessionsController < ApplicationController
       redirect_to signin_path
     else
       session[:user_id] = @user.id
-<<<<<<< HEAD
+      notice: "Welcome back to the theme park!"
       redirect_to user_path(@user)
-=======
-      redirect_to user_path(@user), notice: "Welcome back to the theme park!"
->>>>>>> e8377532b7eb71ab9d63994d832d74e2b92e9d61
     end
   end
 
   def destroy
     session.clear
-    redirect_to controller: 'static_pages', action: 'home'
+    redirect_to root_url
   end
 end
